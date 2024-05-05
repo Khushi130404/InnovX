dd61419579be588fb65a71e56de578d92ef96a79
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-05-05 11:09:53 +0530

    Log2

---
+++ src/main/java/com/MyInnovX/MyDetailsController.java
@@ -0,0 +1,10 @@
+package src.main.java.com.MyInnovX;
+
+public class MyDetailsController {
+
+    public void setDetails(String string, String string2, String string3) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDetails'");
+    }
+
+}
---
+++ src/main/java/com/MyInnovX/Scene2Controller.java
@@ -0,0 +1,10 @@
+package src.main.java.com.MyInnovX;
+
+public class Scene2Controller {
+
+    public void displayName(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayName'");
+    }
+
+}
